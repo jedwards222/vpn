c93deae040b5736e683492f280628db111eebeca
William Wolfe <william.h.wolfe.19@dartmouth.edu>
2017-05-28 00:12:07 -0400

    started client

--- vpncli.py
+++ vpncli.py
@@ -11,8 +11,18 @@ import os       # for os.write, os.read
 import pytun
 import fakenet
 
+
+ifname = 'tun0'
+
+#check to see if a tun iface exists
+ifacelist = os.listdir('/sys/class/net/')
+if not(ifname in ifacelist):
+    print "please create a tun0 interface using openvpn"
+
+
 # Set up tunnel (potentially see Sergey's pytab.open(tap0) - lines 15-17 of pong.py
 # 1. Open tunnel
+tun, ifname = pytun.open('tap0')
 
 # 2. Configure tunnel interface
 
@@ -21,8 +31,11 @@ import fakenet
 
 
 # Process packets going to the tunnel interface
-while 1:
+#while 1:
+
     # Get packet from kernel or from server connection that we need to modify/process
+    #binary_packet = os.read(tun, )
+
 
 
     # Check whether the packet we just read is from our own host, or from the server
