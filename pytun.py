9b1eacc65070a83e795783621db538e1e2657bda
James G. Edwards '18 <jgedwards@flume.cs.dartmouth.edu>
2017-05-27 23:46:38 -0400

    Updated from TAP to TUN in pytun

--- pytun.py
+++ pytun.py
@@ -12,24 +12,23 @@ TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
 IFF_TAP   = 0x0002
 IFF_NO_PI = 0x1000
-TUNMODE = IFF_TAP
+TUNMODE = IFF_TUN       # Should set it to be TUN rather than TAP device
 TUNSETOWNER = TUNSETIFF + 2
 
-# Open TUN device file, create tap0
+# Open TUN device file, create tun0
 #
-#  To open a new transient device, put "tap%d" into ioctl() below.
-#   To open a persistent device, use "tap0" or the actual full name.
+#  To open a new transient device, put "tun%d" into ioctl() below.
+#   To open a persistent device, use "tun0" or the actual full name.
 #
-#  You can create a persistent device with "openvpn --mktun --dev tap0".
+#  You can create a persistent device with "openvpn --mktun --dev tun0".
 #   This device will show up on ifconfig, but will have "no link" unless  
 #   it is opened by this or similar script even if you bring it up with
-#   "ifconfig tap0 up". This can be confusing.
+#   "ifconfig tun0 up". This can be confusing.
 #
-#  Copied from https://gist.github.com/glacjay/585369 
 #   IFF_NO_PI is important! Otherwise, tap will add 4 extra bytes per packet, 
 #     and this will confuse Scapy parsing.
 
-def open(ifname = "tap0"):
+def open(ifname = "tun0"):
     tun = os.open("/dev/net/tun", os.O_RDWR)
     ifs = fcntl.ioctl(tun, TUNSETIFF, struct.pack("16sH", ifname, TUNMODE | IFF_NO_PI))
     granted_ifname = ifs[:16].strip("\x00")  # will be tap0
