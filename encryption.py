a57d6a10525bce0b79232e6d0b4ad0d75469ffb3
James Edwards <james.g.edwards.18@dartmouth.edu>
2017-05-30 21:22:40 -0400

    Add return statements

--- encryption.py
+++ encryption.py
@@ -14,11 +14,13 @@ KEY = '3u5hjgniwm30,xw13--'
 def encrypt(message):
     cyphered = ''.join(chr(ord(c)^ord(k)) for c,k in izip(message, cycle(KEY)))
     # print('%s ^ %s = %s' % (message, KEY, cyphered))
+    return cyphered
 
 # Decryption
 def decrypt(cyphered):
     message = ''.join(chr(ord(c)^ord(k)) for c,k in izip(cyphered, cycle(KEY)))
     # print('%s ^ %s = %s' % (cyphered, KEY, message))
+    return message
 
 def generate_key(size):
     return ''.join(random.SystemRandom().choice(string.ascii_letters + string.punctuation + string.digits) for _ in range(size))
